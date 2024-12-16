e2c9b4dd5f0c5562b9930b36e3761e0abb310bb7
Mahmoud <hotagx3@gmail.com>
2024-12-16 18:45:47 +0400

    Fix deployment issue

---
+++ src/animated-number-react.d.ts
@@ -0,0 +1,8 @@
+// src/animated-number-react.d.ts
+
+declare module 'animated-number-react' {
+    // You can provide a more specific type definition here if needed.
+    const value: any;
+    export = value;
+  }
+  
